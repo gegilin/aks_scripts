8859e7355e898cf6f53772666ed7dd7a156261a0
Gennady Gilin <67386125+gegilin@users.noreply.github.com>
2021-04-06 23:06:32 +0300

    Update kill_pods_cron.sh

--- kill_pods_cron.sh
+++ kill_pods_cron.sh
@@ -1,1 +1,11 @@
-zaza
+#!/bin/sh
+timeout=10
+namespace='services-uksouth'
+echo "Going to kill pods $(date)"
+kubectl -n $namespace get pods --field-selector=status.phase=Running -l 'app.kubernetes.io/name in (core-dms)' | awk '{print $1}' > current_pods.txt
+while IFS= read -r current_pod
+do
+ echo "Going to kill $current_pod"
+ kubectl delete -n $namespace pod $current_pod --force
+done < "current_pods.txt"
+echo "After killing pods $(date)"
